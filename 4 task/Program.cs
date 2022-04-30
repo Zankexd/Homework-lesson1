c0e1001c5dbb1bf4de2ff2ea4b58b80dd89f673e
Oleksii <zankexd@icloud.com>
2022-04-30 23:13:38 +0200

    1 try

---
+++ 4 task/Program.cs
@@ -0,0 +1,19 @@
+﻿int a = new Random().Next(1,50);
+int b = new Random().Next(1,50);
+int c = new Random().Next(1,50);
+
+
+Console.Write (a+",");
+Console.Write (b+",");
+Console.Write (c+",");
+
+int max = a;
+
+if ( a > max ) max = a;
+if ( b > max ) max = b;
+if ( c > max ) max = c;
+
+
+
+Console.WriteLine("max="+max);
+
