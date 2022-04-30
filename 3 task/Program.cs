9fdc2f9a1f237a4e377f435b774fa391d713a90e
Oleksii <zankexd@icloud.com>
2022-04-30 23:09:27 +0200

    1 try

---
+++ 3 task/Program.cs
@@ -0,0 +1,11 @@
+﻿string b = Console.ReadLine();
+int a = int.Parse(b);
+string c = Console.ReadLine();
+int v = int.Parse(c);
+
+if (a<v)
+{
+    Console.WriteLine("max="+v);
+}
+else 
+Console.WriteLine("max="+a);
